30d6c76448e45e28d01f795643cdb8be0eaf6de0
Rifqi-YN <ki771165@gmail.com>
2024-09-22 09:26:24 +0700

    Add files via upload

---
+++ Variable01.java
@@ -0,0 +1,23 @@
+public class Variable01 {
+    public static void main(String[] args) {
+        // deklarasi var
+        String Nama;
+        int Umur;
+
+        // Input nilai
+        Nama = "Muhammad Rifqi";
+        Umur = 19;
+
+
+        // output ke layar
+        System.out.print("nama = ");
+        System.out.println(Nama);
+        System.out.print("umur = ");
+        System.out.println(Umur);
+        
+
+
+
+    }
+    
+}
